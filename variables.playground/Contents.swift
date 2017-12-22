2d49ea5056d5ef6c826ebd6a292a237ac84bab1d
Anahi Salgado <anahi@platzi.com>
2017-12-21 20:19:50 -0600

    variables

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+// Variables
+
+var number1 : Int
+number1 = 50
+
+var number2: Int = 100
+
+var number3 = 1000
+
+// constantes
+let gravity = 9.81
+//gravedad = 11.0
