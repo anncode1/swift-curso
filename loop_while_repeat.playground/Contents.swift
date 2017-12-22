66870b8f57e22d244d4b6fa9f9ef764a1a063722
Anahi Salgado <anahi@platzi.com>
2017-12-21 20:24:55 -0600

    while loop

---
+++ loop_while_repeat.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+// While
+var i = 1
+while i <= 100{
+    print(i)
+    i = i + 1
+}
+
+//Repeat
+//var i = 1
+//repeat{
+//    print(i)
+//    i = i + 1
+//} while i <= 100
