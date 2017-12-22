49ffbc2123b74c2e1a9f318b44bca75e1bcb47fe
Anahi Salgado <anahi@platzi.com>
2017-12-21 21:10:34 -0600

    control acceso

---
+++ control_de_acceso.playground/Contents.swift
@@ -0,0 +1,5 @@
+//: Playground - noun: a place where people can play
+// Control de acceso
+
+
+var cursoSwift = Curso(nombre : "Curso de Swift", imagen : "platzi.com/swift.png")
---
+++ control_de_acceso.playground/Sources/Cursos.swift
@@ -0,0 +1,9 @@
+public class Curso{
+    public var nombre : String
+    public var imagen : String
+    
+    public init(nombre : String, imagen : String){
+        self.nombre = nombre
+        self.imagen = imagen
+    }
+}
