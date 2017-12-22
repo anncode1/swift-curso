3a68ac2bb11a2f76db6a4fb8aac6045e1ae915e8
Anahi Salgado <anahi@platzi.com>
2017-12-21 21:02:52 -0600

    protocolos

---
+++ protocolos.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+// Protocolos
+protocol miProtocolo{
+    func numeroAleatorio()
+}
+
+class sorteo : miProtocolo{
+    func numeroAleatorio(){
+        // generar numero
+    }
+}
+
+
